c9010e5b331567c8e6d4e3c4af4dab1512ab39c2
anumsajid13 <anumsajid13@gmail.com>
2025-02-18 14:16:01 +0500

    hahhahaahaa

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Deploying Flask App at Vercel"
+    return "Deploying Lab Task at Vercel"
 
 if __name__ == "__main__":
     import os
